5e4fc795e6a0f76d12c3a9e85cefa28a1171591b
TiAd <47915873+TiAd-GT@users.noreply.github.com>
2020-02-14 16:15:31 +0800

    Add files via upload

---
+++ js/tiaozhuan.js
@@ -0,0 +1,24 @@
+function open_jkfz(){
+    window.open("http://www.jkfz.cn","_blank","toolbar=yes, location=yes, directories=no, status=no, menubar=yes, scrollbars=yes, resizable=no, copyhistory=yes, width=1000, height=600");
+}
+function open_jyb(){
+    window.open("http://www.moe.gov.cn/","_blank","toolbar=yes, location=yes, directories=no, status=no, menubar=yes, scrollbars=yes, resizable=no, copyhistory=yes, width=1000, height=600");
+}
+function open_lj1(){
+    window.open("http://www.moe.gov.cn/srcsite/A06/s3321/201812/t20181229_365360.html","_blank","toolbar=yes, location=yes, directories=no, status=no, menubar=yes, scrollbars=yes, resizable=no, copyhistory=yes, width=1000, height=600");
+}
+function open_lj2(){
+    window.open("http://www.moe.gov.cn/jyb_xwfb/gzdt_gzdt/s5987/201712/t20171205_320606.html","_blank","toolbar=yes, location=yes, directories=no, status=no, menubar=yes, scrollbars=yes, resizable=no, copyhistory=yes, width=1000, height=600");
+}
+function open_lj3(){
+    window.open("http://www.moe.gov.cn/srcsite/A02/s7049/201908/t20190819_394973.html","_blank","toolbar=yes, location=yes, directories=no, status=no, menubar=yes, scrollbars=yes, resizable=no, copyhistory=yes, width=1000, height=600");
+}
+function open_lj4(){
+    window.open("http://www.moe.gov.cn/jyb_xxgk/moe_1777/moe_1778/201701/t20170119_295319.html","_blank","toolbar=yes, location=yes, directories=no, status=no, menubar=yes, scrollbars=yes, resizable=no, copyhistory=yes, width=1000, height=600");
+}
+function open_lj5(){
+    window.open("http://www.moe.gov.cn/srcsite/A06/s3321/201901/t20190102_365728.html","_blank","toolbar=yes, location=yes, directories=no, status=no, menubar=yes, scrollbars=yes, resizable=no, copyhistory=yes, width=1000, height=600");
+}
+function open_lj6(){
+    window.open("http://www.moe.gov.cn/s78/A03/moe_560/jytjsj_2018/","_blank","toolbar=yes, location=yes, directories=no, status=no, menubar=yes, scrollbars=yes, resizable=no, copyhistory=yes, width=1000, height=600");
+}
